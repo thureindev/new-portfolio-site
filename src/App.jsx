bedbb71b887b1b807d34fd615a62efe761e2eefb
thureindev <thurein.dev@gmail.com>
2023-07-14 21:36:18 +0630

    testing hashrouter on github

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+// This is a React Router v6 app
+import {
+    BrowserRouter,
+    HashRouter,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+
+import Home from './Home'
+import Projects from './Projects'
+import Blog from './Blog'
+import Contact from './Contact'
+
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Navbar></Navbar>
+            {/* Because GH Pages doen not work well with BrowserRouter */}
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    {/* <Route path="users/*" element={<Projects />} /> */}
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </HashRouter>
+        </>
+    )
 }
 
 export default App
